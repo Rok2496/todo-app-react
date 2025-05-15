cf9d0fc56dbdc1ee7c0ecacb993bdfee29767bb3
rok2496 <mdrokon@hotmail.com>
2025-05-15 19:50:34 +0600

    update

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000';
+const API_URL = 'https://fast-api-todo-e9rg.onrender.com/';
 
 // Create axios instance
 const api = axios.create({
